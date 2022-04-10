26fbec60a74f4e218933394024b0f5b601a135f4
dhrn <dharan@vyaparapp.in>
2022-04-11 00:30:55 +0530

    [minor] minor improvements

--- apps/ng-mailer/src/app/app.module.ts
+++ apps/ng-mailer/src/app/app.module.ts
@@ -6,11 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { MailConfigurationComponent } from './mail-configuration/mail-configuration.component';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DynamicFormsMaterialUIModule } from "@ng-dynamic-forms/ui-material";
 
 @NgModule({
   declarations: [AppComponent, MailConfigurationComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, MonacoEditorModule.forRoot(), FormsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, MonacoEditorModule.forRoot(), FormsModule, ReactiveFormsModule, DynamicFormsMaterialUIModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/ng-mailer/src/app/mail-configuration/mail-configuration.component.ts
+++ apps/ng-mailer/src/app/mail-configuration/mail-configuration.component.ts
@@ -1,20 +1,106 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+import {
+  DynamicFormModel,
+  DynamicCheckboxModel,
+  DynamicInputModel,
+  DynamicFormService,
+  DynamicFormGroupModel
+} from "@ng-dynamic-forms/core";
+
+export const MY_FORM_MODEL: DynamicFormModel = [
+
+  new DynamicInputModel({
+      id: "host",
+      label: "Host",
+      placeholder: "Mail Host",
+      additional: {
+        appearance: "fill"
+      }
+  }),
+
+  new DynamicInputModel({
+    id: "port",
+    label: "Port",
+    placeholder: "Mail Port",
+    additional: {
+      appearance: "fill"
+    }
+  }),
+
+
+  new DynamicFormGroupModel({
+    id: "auth",
+    legend: "Authentication",
+    group: [
+        new DynamicInputModel({
+            id: "user",
+            label: "User Name",
+            additional: {
+              appearance: "fill"
+            }
+        }),
+        new DynamicInputModel({
+            id: "pass",
+            label: "Password",
+            additional: {
+              appearance: "fill"
+            }
+        })
+    ]
+}),
+
+  new DynamicCheckboxModel({
+      id: "secureConnection",
+      label: "Secure Connection",
+      additional: {
+        appearance: "fill"
+      }
+  })
+];
 @Component({
   selector: 'mailer-poc-mail-configuration',
   templateUrl: './mail-configuration.component.html',
-  styleUrls: ['./mail-configuration.component.scss']
+  styleUrls: ['./mail-configuration.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MailConfigurationComponent {
+  TABS = { 1: "FORM", 2: "EDITOR" };
   editorOptions = {theme: 'vs-dark', language: 'json'};
+  currentTab: 1 | 2 = 1;
+  formModel: DynamicFormModel = MY_FORM_MODEL;
+  formGroup: FormGroup;
 
   constructor(
+    private formService: DynamicFormService,
     public dialogRef: MatDialogRef<MailConfigurationComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-  ) {}
+  ) {
+   this.formGroup = this.formService.createFormGroup(this.formModel);
+   this.formGroup.patchValue(JSON.parse(data));
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  close() {
+    const TAB_NAME = this.TABS[this.currentTab] ?? "FORM";
+    if (TAB_NAME === "FORM") {
+      this.data = JSON.stringify(this.formGroup.value, null, 4);
+      this.dialogRef.close(this.data);
+    } else {
+      this.dialogRef.close(this.data);
+    }
+  }
+
+  resize(event: any) {
+    this.currentTab = event.index;
+    this.data = JSON.stringify(this.formGroup.value, null, 4);
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+  }, 200);
   }
 }
